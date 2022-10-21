ff0500b8593269bec563ad7aaca460c84ece1f3c
Nelson Gauthier <nelson.gauthier@gmail.com>
2022-10-21 10:20:11 -0700

    Autoreload hammerspoon config on change

--- home/dot_hammerspoon/init.lua
+++ home/dot_hammerspoon/init.lua
@@ -1,1 +1,15 @@
 hs.window.animationDuration = 0 -- disable animations
+
+--
+-- Autoreload config on change
+--
+
+function reloadConfig(files)
+  for _, file in pairs(files) do
+    if file:sub(-4) == '.lua' then
+      hs.reload()
+    end
+  end
+end
+
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
