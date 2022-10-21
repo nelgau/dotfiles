cd14becc55907f28b4578106b9063454cf63d857
Nelson Gauthier <nelson.gauthier@gmail.com>
2022-10-21 10:28:03 -0700

    Initial hammerspoon bindings for app switching

--- home/dot_hammerspoon/init.lua
+++ home/dot_hammerspoon/init.lua
@@ -1,6 +1,33 @@
 hs.window.animationDuration = 0 -- disable animations
 
 --
+-- Key bindings
+--
+
+function launchOrFocus(app)
+  return function()
+    -- FIXME: If the iTerm hotkey window is open, close it.
+    hs.application.launchOrFocus(app)
+  end
+end
+
+local bindings = {
+  [{'alt', 'cmd', 'ctrl', 'shift'}] = {
+    c = launchOrFocus('Google Chrome'),
+    f = launchOrFocus('Finder'),
+    i = launchOrFocus('Visual Studio Code'),
+    o = launchOrFocus('Sublime Text'),
+    y = launchOrFocus('System Preferences'),
+  },
+}
+
+for modifier, keyActions in pairs(bindings) do
+  for key, action in pairs(keyActions) do
+    hs.hotkey.bind(modifier, tostring(key), action)
+  end
+end
+
+--
 -- Autoreload config on change
 --
 
