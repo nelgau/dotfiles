56edb705571d87062e87a2399a079929372a44c3
Nelson Gauthier <nelson.gauthier@gmail.com>
2022-11-07 00:06:26 -0800

    nvim: Migrate from airline to lualine

--- home/dot_config/nvim/after/plugin/init.lua
+++ home/dot_config/nvim/after/plugin/init.lua
@@ -1,2 +1,1 @@
 vim.g.NERDTreeShowHidden = 1
-vim.g.airline_powerline_fonts = 1
--- home/dot_config/nvim/lua/nelgau/packer.lua
+++ home/dot_config/nvim/lua/nelgau/packer.lua
@@ -1,11 +1,12 @@
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
+    use 'nvim-lualine/lualine.nvim'
+
     -- use "preservim/nerdtree"
     use {'santiagovrancovich/nerdtree', branch = 'hotfix-issue-#1321'}
 
     use 'tpope/vim-commentary'
-    use 'vim-airline/vim-airline'
     use 'rafi/awesome-vim-colorschemes'
     use 'tc50cal/vim-terminal'
     use {'neoclide/coc.nvim', branch = 'release'}
