0716fdc695dea1f350a57af9de4f1a56915e81a6
Nelson Gauthier <nelson.gauthier@gmail.com>
2022-11-11 14:02:25 -0800

    nvim: Disable side column inside NERDTree

--- home/dot_config/nvim/after/plugin/nerdtree.lua
+++ home/dot_config/nvim/after/plugin/nerdtree.lua
@@ -2,3 +2,10 @@ vim.g.NERDTreeShowHidden = 1
 
 vim.g.NERDTreeDirArrowExpandable = '+'
 vim.g.NERDTreeDirArrowCollapsible = '~'
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "nerdtree",
+  callback = function()
+    vim.opt_local.signcolumn = "no"
+  end,
+})
